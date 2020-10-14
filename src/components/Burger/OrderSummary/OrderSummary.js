8f08b7ef2a888537da5e32c4c3d850fb44346f57
Mark <youremail@gmail.comg>
2020-10-14 15:14:53 -0600

    added backdrop component and animation

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import Auxiliary from "../../../hoc/Auxiliary";
+import Button from "../../UI/Button/Button";
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+            <li key={igKey}>
+                <span style={{textTransform: "capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}
+            </li>
+            );
+        })
+    return (
+        <Auxiliary>
+            <h3>Your Order</h3>
+            <p>A nice burg with this stuff on it:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCanceled}>Cancel</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
+        </Auxiliary>
+    )
+
+
+};
+
+export default orderSummary;
