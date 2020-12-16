91393e0f120bbe29f32143bd72d852e090fdaa0b
Mark <youremail@gmail.comg>
2020-12-16 16:03:45 -0700

    added updateObject utility function to contact/auth form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import Button from "../../components/UI/Button/Button";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import classes from "./Auth.css";
 import * as actions from "../../store/actions/index";
+// import { updateObject } from "../../shared/utility";
 
 class Auth extends Component {
   state = {
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,5 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
-import { updateObject } from "../utility";
+import { updateObject } from "../../shared/utility";
 
 const initialState = {
     orders: [],
@@ -25,7 +25,7 @@ const purchaseBurgerSuccess = (state, action) => {
 };
 
 const purchaseBurgerFail = (state, action) => {
-    return updateObject(state, {loading: false});
+    return updateObject(state, {loading: true});
 };
 
 const fetchOrdersStart = (state, action) => {
